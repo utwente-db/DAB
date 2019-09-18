f8aa3c63c11fc2f2132619b080b181720c5d4cbe
fpjnijweide <fpjnijweide@protonmail.com>
2019-09-18 11:59:07 +0200

    almost fixed making things static correctly

--- designproject/settings.py
+++ designproject/settings.py
@@ -133,6 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = "static"
 STATIC_URL = '/static/'
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
