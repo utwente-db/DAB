536d2d4c3a291d5865ff0ac850b1744665f44aae
fpjnijweide <fpjnijweide@protonmail.com>
2019-09-18 12:10:07 +0200

    static things work!!!

--- designproject/settings.py
+++ designproject/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = "staticroot"
+STATIC_ROOT = "static"
 STATIC_URL = '/static/'
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
