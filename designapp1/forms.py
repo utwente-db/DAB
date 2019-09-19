8bcaff1ddb2659d19a433e4c665ff5f378af1744
DenysRF <denysflederus@gmail.com>
2019-09-19 23:41:38 +0200

    register done

--- designapp1/forms.py
+++ designapp1/forms.py
@@ -2,9 +2,31 @@ from django import forms
 
 class RegisterForm(forms.Form):
 	#TODO: validate emails are emails via regex (possibly do this in frontend)
-	mail = forms.CharField(max_length=256, required=True, label="email")
-	password = forms.CharField(widget=forms.PasswordInput, required=True, label="password")
-	passwordConfirm = forms.CharField(widget=forms.PasswordInput, required=True, label="confirm password")
+	mail = forms.CharField(
+		widget=forms.TextInput(
+			attrs={
+				'class': 'form-control',
+				# 'placeholder': 'your.name@student.utwente.nl'
+				'id': 'email',
+				'autofocus': 'True'
+			}
+		), required=True, label='email', max_length=256
+	)
+	password = forms.CharField(
+		widget=forms.PasswordInput(
+			attrs={
+				'class': 'form-control',
+				'id': 'password'
+			}
+		), required=True, label='password')
+
+	passwordConfirm = forms.CharField(
+		widget=forms.PasswordInput(
+			attrs={
+				'class': 'form-control',
+				'id': 'passwordConfirm'
+			}
+		), required=True, label="confirm password")
 
 	def validate(self):
 		data = self.cleaned_data
@@ -19,14 +41,14 @@ class LoginForm(forms.Form):
 				'class': 'form-control mb-4',
 				'placeholder': 'E-Mail Address',
 				'autofocus': 'True'
-				}),
-		max_length=100, required=True, label=""
+			}
+		), max_length=100, required=True, label=''
 	)
 	password = forms.CharField(
 		widget=forms.PasswordInput(
 			attrs={
 				'class': 'form-control mb-4',
 				'placeholder': 'Password',
-			}),
-		required=True, label=""
+			}
+		), required=True, label=''
 	)
