2c7f35c981418d2d4089d5ae3b96c4fc3aa0f817
fpjnijweide <fpjnijweide@protonmail.com>
2019-09-25 17:09:28 +0200

    css almost working

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const ts_loader = require('ts-loader');
 var glob = require("glob");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // function getPlugin() {
 //     if (process.env.NODE_ENV === 'production') {
@@ -15,12 +16,18 @@ var glob = require("glob");
 
 config = {
     mode: 'development', // TODO change to production when in production or let freek add a new "npm run" thing
-    entry:
-        glob.sync("./src/frontend/scripts/*.ts"), //.concat(glob.sync("./src/frontend/sass/*.s*ss")),
+    // plugins: [
+    //     new ExtractTextPlugin('[name].css')
+    // ],
+    entry: {
+        'yeeter/scripts/main.js': glob.sync("./src/frontend/scripts/*.ts"),
+        'yeeter/css/style.css': glob.sync("./src/frontend/sass/*.s*ss")
+    },
+
 
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './yeeter/')
+        filename: '[name]',
+        path: path.resolve(__dirname, './')
     },
     devtool: 'source-map', // TODO comment out in production
     resolve: {
@@ -43,19 +50,32 @@ config = {
                 test: /\.ts$/,
                 use: 'ts-loader'
             },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+
+            }
             // {
             //     // include: path.resolve(__dirname, '/src/frontend/scripts/'),
             //     test: /\.s.ss$/,
-            //     use: [
-            //         'style-loader',
-            //         {
-            //             loader: 'css-loader',
-            //             options: {
-            //                 minimize: true
-            //             }
-            //         },
-            //         'sass-loader?sourceMap'
-            //     ]
+            //     loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!sass-loader?sourceMap')
+            //     // use: [
+            //     //     'style-loader',
+            //     //     {
+            //     //         loader: 'css-loader',
+            //     //         options: {
+            //     //             minimize: true
+            //     //         }
+            //     //     },
+            //     //     'sass-loader?sourceMap'
+            //     // ]
             // }
 
 
