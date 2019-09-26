778da2ad82dcd361c27cf76bf2b17c50727b8e3d
fpjnijweide <fpjnijweide@protonmail.com>
2019-09-26 12:26:10 +0200

    renamed login urls to urls

--- designproject/urls.py
+++ designproject/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rest/', include('designapp1.rest_urls')),
-    url('', include('designapp1.login_urls'))
+    url('', include('designapp1.urls'))
 ]
