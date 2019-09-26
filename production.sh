098acdecc01f5ccf4130f1bf557f4fde3e3ed2cc
notinecrafter <bevestigding@outlook.com>
2019-09-26 13:27:08 +0200

    aaaa

--- production.sh
+++ production.sh
@@ -3,7 +3,7 @@
 source ../venv/bin/activate
 npm run production
 find . -name "*.map" -type f|xargs rm -f
-./manage.py collectstatic
+./manage.py collectstatic --noinput
 
 # floris' other commands go here
 
