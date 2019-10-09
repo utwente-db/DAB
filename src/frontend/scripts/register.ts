fdac731a49145196ded3f99e39bdab8ae3894c7c
fpjnijweide <fpjnijweide@protonmail.com>
2019-10-09 10:21:50 +0200

    register actually works

--- src/frontend/scripts/register.ts
+++ src/frontend/scripts/register.ts
@@ -88,9 +88,9 @@ async function tryRegister(): Promise<void> {
         const credentials: Credentials = {email: registerEmailField.value, password: registerPasswordField.value};
         const tempAlert: ChildNode | null = addTempAlert();
         try {
-            // const response: AxiosResponse = await axios.post(DBMusers, credentials); // TODO fix type with interface
-            // const database: responeType = await response.data;
-            // addAlert(`that worked`, AlertType.success, tempAlert)
+            const response: AxiosResponse = await axios.post("/rest/dbmusers/", credentials); // TODO fix type with interface
+            const responseData: string = await response.data;
+            addAlert(`Please check your inbox to confirm your e-mail`, AlertType.success, tempAlert)
             // TODO redirect
         } catch (error) {
             addErrorAlert(error, tempAlert)
