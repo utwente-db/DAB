9e1b790c392f36548af2d6e39768f1d4f935ffe5
fpjnijweide <fpjnijweide@protonmail.com>
2019-10-10 22:47:53 +0200

    dump button style is nice

--- src/frontend/scripts/student_view.ts
+++ src/frontend/scripts/student_view.ts
@@ -59,16 +59,17 @@ async function populateHaveCredentialsPane(i: number) {
                             </div>
                         </div>
                         <div class="align-items-center align-items-stretch row">
-                            <div class="center-block col-12 col-md-4 my-2 my-md-4">
-                                <button id="delete-button-${db.dbid}" class="btn btn-danger delete-button ">Delete database and release credentials</button>
+                            <div class="center-block col-12 col-md-4 my-2 my-md-4 d-flex">
+                                <button id="delete-button-${db.dbid}" class="btn btn-danger delete-button btn-block">Delete database and release credentials</button>
                             </div>
-                            <div class="center-block col-12 col-md-4 my-2 my-md-4"">
-                                <button id="reset-button-${db.dbid}" class="btn btn-info reset-button">Reset database</button>
+                            <div class="center-block col-12 col-md-4 my-2 my-md-4 d-flex">
+                                <button id="reset-button-${db.dbid}" class="btn btn-info reset-button btn-block">Reset database</button>
                             </div>
-                            <div class="center-block col-12 col-md-4 my-2 my-md-4"">
-                                <a id="dump-button-${db.dbid}" href="/rest/dump/${db.dbid}" target="_blank" class="btn btn-secondary dump-button">Get dump of database</a>
+                            <div class="center-block col-12 col-md-4 my-2 my-md-4 d-flex">
+                                <a class="d-flex" href="/rest/dump/${db.dbid}" target="_blank"><button id="dump-button-${db.dbid}" class="btn-block d-flex btn btn-secondary dump-button">Get dump of database</button></a>
                             </div>
                         </div>`;
+            // TODO make third button mobile-friendly
             credentials += html.trim();
             dbIDs.push(db.dbid)
         }
