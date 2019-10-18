20a62020efac86cf3e4b82cd0d4d40034bcf5e70
fpjnijweide <fpjnijweide@protonmail.com>
2019-10-18 21:15:31 +0200

    removed django forms from register page; added hints to login page

--- src/frontend/scripts/login.ts
+++ src/frontend/scripts/login.ts
@@ -1,5 +1,6 @@
 import {addAlert, addErrorAlert, addTempAlert, AlertType} from "./alert";
 import axios from "axios";
+import {validEmail, validPassword} from "./register";
 
 const djangoTemplate = document.getElementById("django-template") as HTMLTemplateElement;
 const emailInput = document.getElementById("email-input") as HTMLInputElement;
@@ -7,6 +8,7 @@ const passwordInput = document.getElementById("password-input") as HTMLInputElem
 const loginButton = document.getElementById("login-button") as HTMLButtonElement;
 const registerLink = document.getElementById("register-link") as HTMLAnchorElement;
 const forgotPasswordLink = document.getElementById("forgot-password-link") as HTMLAnchorElement;
+const loginForm = document.getElementById("login-form") as HTMLFormElement;
 
 function disableInputs() {
     emailInput.disabled = true;
@@ -26,7 +28,7 @@ function enableInputs() {
 
 async function tryResendVerificationEmail(tempAlert: ChildNode | null) {
 
-    disableInputs()
+    disableInputs();
     const userEmail: string = djangoTemplate.classList[1];
     if (tempAlert && document.body.contains(tempAlert)) {
         tempAlert.remove();
@@ -39,11 +41,35 @@ async function tryResendVerificationEmail(tempAlert: ChildNode | null) {
         addErrorAlert(error, newTempAlert)
     } finally {
         enableInputs()
-
     }
 
 }
 
+
+function checkFields() {
+    const a = validEmail(emailInput);
+    const b = validPassword(passwordInput);
+    return a && b;
+}
+
+function tryLogin() {
+    if (checkFields()) {
+        loginForm.submit();
+        // disableInputs();
+        // const userEmail: string = djangoTemplate.classList[1];
+        //
+        // const tempAlert: ChildNode | null = addTempAlert();
+        // try {
+        //     await axios.post(`/resend_verification/`, {'email': userEmail});
+        //     addAlert(`Please check your inbox to confirm your e-mail`, AlertType.success, newTempAlert)
+        // } catch (error) {
+        //     addErrorAlert(error, newTempAlert)
+        // } finally {
+        //     enableInputs()
+        // }
+
+    }
+}
 
 window.onload = () => {
 
@@ -55,5 +81,12 @@ window.onload = () => {
 
             tryResendVerificationEmail(tempAlert);
         })
+    }
+
+    if (loginForm) {
+        loginForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            tryLogin();
+        });
     }
 }
