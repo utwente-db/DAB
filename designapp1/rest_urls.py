8f4023493190896297aea64e6bd341b2dbe9221e
s1578197 <r.h.h.g.m.linssen@student.utwente.nl>
2019-10-24 12:13:00 +0200

    rest_urls regex staat goed, _ wordt ondersteund in zoeken bij courses. REST CALL: /rest/studentdatabases/name/<name>

--- designapp1/rest_urls.py
+++ designapp1/rest_urls.py
@@ -17,15 +17,15 @@ urlpatterns = [
     url(r'^studentdatabases/own/?$', views.get_own_response, {"dbname": "studentdatabases"}),
     url(r'^studentdatabases/(?P<pk>[a-z0-9]+)/?$', views.singleview, {"dbname": "studentdatabases"}),
     url(r'^studentdatabases/$', views.baseview, {"dbname": "studentdatabases"}),
-    url(r'^studentdatabases/name/(?P<search_value>[a-zA-Z0-9]+)/?$', views.search_on_name, {"dbname": "studentdatabases"}),
+    url(r'^studentdatabases/name/(?P<search_value>[a-zA-Z0-9_]+)/?$', views.search_on_name, {"dbname": "studentdatabases"}),
     url(r'^studentdatabases/owner/(?P<search_value>[0-9]+)/?$', views.search_on_owner, {"dbname": "studentdatabases"}),
     url(r'^studentdatabases/course/(?P<search_value>[0-9]+)/?$', views.search_db_on_course),
     # Courses
     url(r'^courses/own/?$', views.get_own_response, {"dbname": "courses"}),
     url(r'^courses/(?P<pk>[0-9]+)/schema/?', views.schema),
-    url(r'^courses/(?P<pk>[a-z0-9]+)/?$', views.singleview, {"dbname": "courses"}),
+    url(r'^courses/(?P<pk>[0-9]+)/?$', views.singleview, {"dbname": "courses"}),
     url(r'^courses/$', views.baseview, {"dbname": "courses"}),
-    url(r'^courses/name/(?P<search_value>[a-z0-9]+)/?$', views.search_on_name, {"dbname": "courses"}),
+    url(r'^courses/name/(?P<search_value>[a-zA-Z0-9_]+)/?$', views.search_on_name, {"dbname": "courses"}),
     # TAs
     url(r'^tas/own/?$', views.get_own_response, {"dbname": "tas"}),
     url(r'^tas/teacher/own/?$', views.teacher_own_tas, {"dbname": "tas"}),
@@ -37,7 +37,7 @@ urlpatterns = [
     #  url(r'^studentdatabases/name/(?P<search_value>[a-z0-9]+)/?$', views.search_on_name,{"dbname":"tas"}),
     # dbmusers
     url(r'^dbmusers/own/?$', views.get_own_response, {"dbname": "dbmusers"}),
-    url(r'^dbmusers/(?P<pk>[a-z0-9]+)/?$', views.singleview, {"dbname": "dbmusers"}),
+    url(r'^dbmusers/(?P<pk>[0-9]+)/?$', views.singleview, {"dbname": "dbmusers"}),
     url(r'^dbmusers/$', views.baseview, {"dbname": "dbmusers"}),
     url(r'^dbmusers/email/(?P<search_value>[a-zA-Z0-9@.]+)/?$', views.search_on_name, {"dbname": "dbmusers"}),
     url(r'^dbmusers/course/(?P<pk>[0-9]+)/?$', views.search_dbmusers_on_course),
