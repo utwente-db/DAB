2fad3754e19533bde0a1b17296466f36981dabef
notinecrafter <bevestigding@outlook.com>
2019-10-29 10:46:38 +0100

    add default admin account

--- migrate.sh
+++ migrate.sh
@@ -7,3 +7,4 @@ export DB_PORT=$(cat django_settings/secret.py | grep db_port | sed "s/db_port =
 export PGPASSWORD=$(cat django_settings/secret.py | grep db_password | sed "s/db_password = //" | sed 's/"//g' | sed "s/'//g")
 
 cat schema.sql | psql -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME
+echo "INSERT INTO dbmusers(role, email, password, verified) VALUES(0, 'info@utwente.nl', 'sha3_256\$HcbFtxZw\$7y5cYH+mkK1t7gms/BPo8nWqXewQ9pSO/8VTPTDbchg=', true);" | psql -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME
