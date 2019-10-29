4fb3971325bea18494b37e6ba499735fc40d3821
notinecrafter <bevestigding@outlook.com>
2019-10-29 15:59:54 +0100

    changed mail for created accounts

--- src/backend/mail.py
+++ src/backend/mail.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 # TODO: configure hosts correctly and wire them into here
 # TODO: put the product name in here once we come up with one
-def send_verification(user):
+def send_verification(user, created=False):
     a = user["email"].split("@")
     addr = Address(user["email"], a[0], a[1])
 
@@ -24,17 +24,49 @@ def send_verification(user):
     msg.add_alternative("""\
 		<html>
 			<body>
-				<p>Hello """ + user["email"] + """,</p>
-				<p>You have requested an account for the Database Administration Bundle.</p>
-				<p>Please verify your email by using <a href='https://""" + settings.HOST_SERVER + """/verify/""" +
+				<p>Hello """ + user["email"] + """,</p>"""+
+				"""<p>You have requested an account for the Database Administration Bundle.</p>""" if not created else """<p>An account hase been requested for you at the Database Administration Bundle</p>"""
+				+"""<p>Please verify your email by using <a href='https://""" + settings.HOST_SERVER + """/verify/""" +
                         user["token"] + """'>this link</a>.
 				<p>Kind Regards,</p>
 				<p>Database Administration Bundle</p>
 			</body>
-		<html>""".format(cid=cid[1:-1]), subtype='html')
+		</html>
+        """.format(cid=cid[1:-1]), subtype='html')
 
     if not settings.DEBUG:
-        mailer = smtplib.SMTP(settings.EMAIL_SERVER);
+        mailer = smtplib.SMTP(settings.EMAIL_SERVER)
+        mailer.send_message(msg)
+    else:
+        print(msg)
+
+def send_creation_notice(user):
+    a = user["email"].split("@")
+    addr = Address(user["email"], a[0], a[1])
+
+    msg = EmailMessage()
+    msg["Subject"] = "Database Administration Bundle - An account has been made for you"
+    msg["From"] = settings.EMAIL_SENDER
+    msg["To"] = addr
+    msg.set_content(
+        "An account has been made for this email at the Database Administration Bundle. If this does not ring a bell, you can safel ignore this message.\n\
+        If it does, the administrator should have already contacted you. You can log in via https://"+settings.HOST_SERVER+"/")
+    cid = make_msgid()
+    msg.add_alternative("""\
+        <html>
+            <body>
+                <p>Hello """+user["email"]+""",</p>
+                <p>Somebody has made an account for you at the Database Administration Bundle. If this does not ring a bell, you can safely ignore this message.</p>
+                <p>If it does, the administrator should have already contacted you. You can log in <a href='https://"""+settings.HOST_SERVER+"""/'>here</a>.</p>
+                <p>Your email has already been verified.</p>
+                <p>Kind Regards,</p>
+                <p>Database Administration Bundle</p>
+            </body>
+        </html>
+        """.formad(cid=cid[1:-1]), subtype='html')
+
+    if not settings.DEBUG:
+        mailer = smtplib.SMTP(settings.EMAIL_SERVER)
         mailer.send_message(msg)
     else:
         print(msg)
@@ -68,7 +100,7 @@ def send_reset(user):
         """.format(cid=cid[1:-1]), subtype='html')
 
     if not settings.DEBUG:
-        mailer = smtplib.SMTP(settings.EMAIL_SERVER);
+        mailer = smtplib.SMTP(settings.EMAIL_SERVER)
         mailer.send_message(msg)
     else:
         print(msg)
