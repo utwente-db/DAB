f68722427a66b4018b1168a7323a6475c1dc05e4
fpjnijweide <fpjnijweide@protonmail.com>
2019-11-01 13:29:59 +0100

    added typedoc to change_password.ts

--- src/frontend/scripts/change_password.ts
+++ src/frontend/scripts/change_password.ts
@@ -4,19 +4,27 @@ import {passwordsEqual, validPassword} from "./register";
 import {changePageState, initNavbar} from "./navbar";
 
 const newPasswordButton = document.getElementById("new-password-button") as HTMLButtonElement;
-
 const oldPasswordField = document.getElementById("old-password-field") as HTMLInputElement;
 const newPasswordField = document.getElementById("new-password-field") as HTMLInputElement;
 const confirmPasswordField = document.getElementById("confirm-password-field") as HTMLInputElement;
 const content = document.getElementById('content') as HTMLFormElement;
 
-function checkFields(): boolean {
+/**
+ * Checks the fields on the change password page.
+ * Defines all the constants separately to avoid lazy evaluation of non-pure functions.
+ * [[validPassword]] and [[passwordsEqual]] add feedback for the user to the form, so we must avoid lazy evaluation
+ */
+function checkChangePasswordFields(): boolean {
     const a = validPassword(oldPasswordField); // Can't use a one-line function here due to lazy evaluation
     const b = validPassword(newPasswordField);
     const c = passwordsEqual(newPasswordField, confirmPasswordField);
     return a && b && c
 }
 
+/**
+ * Disables or enables all the elements on the page
+ * @param enable If true, enables elements. Else, disables elements.
+ */
 function changeChangePasswordState(enable: boolean): void {
     if (enable) {
         newPasswordField.disabled = false;
@@ -24,7 +32,6 @@ function changeChangePasswordState(enable: boolean): void {
         confirmPasswordField.disabled = false;
         newPasswordButton.disabled = false;
         (document.getElementById("navbar-change-password") as HTMLAnchorElement)!.classList.add("disabled")
-
     } else {
         newPasswordField.disabled = true;
         oldPasswordField.disabled = true;
@@ -33,8 +40,11 @@ function changeChangePasswordState(enable: boolean): void {
     }
 }
 
+/**
+ * Checks if all the inputs are correct, disables page elements, tries to reset the password, and adds alerts
+ */
 async function tryResetPassword(): Promise<void> {
-    if (checkFields()) {
+    if (checkChangePasswordFields()) {
         changePageState(false, changeChangePasswordState);
         const tempAlert: ChildNode | null = addTempAlert();
         try {
@@ -60,6 +70,10 @@ async function tryResetPassword(): Promise<void> {
     }
 }
 
+/**
+ * Initiates navbar (see [[initNavbar]] ) and adds event listeners.
+ * Disables the "change password" link in the navbar because we are already on that page.
+ */
 window.onload = () => {
     initNavbar(changeChangePasswordState);
     content.addEventListener("submit", (event) => {
