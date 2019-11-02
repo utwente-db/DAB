1b5dccc4c3a820fd776464859ecd6e254a9d3484
fpjnijweide <fpjnijweide@protonmail.com>
2019-11-02 14:17:26 +0100

    documented + refactored navbar

--- src/frontend/scripts/navbar.ts
+++ src/frontend/scripts/navbar.ts
@@ -15,35 +15,53 @@ export const navbarEditUsers = document.getElementById("navbar-edit-users") as H
 export const navbarChangePasswordLink = document.getElementById("navbar-change-password") as HTMLAnchorElement;
 export const navbarLogoutLink = document.getElementById("navbar-logout") as HTMLAnchorElement;
 export const navbarDumpAllDatabasesLink = document.getElementById("navbar-dump-all-databases") as HTMLAnchorElement;
-export const navbarMissingDatabasesLink = document.getElementById("navbar-missing-databases") as HTMLAnchorElement;
-export const missingDatabasesSelect = document.getElementById("selected-missing-database") as HTMLSelectElement;
-export const deleteMissingDatabaseButton = document.getElementById("delete-missing-database-button") as HTMLButtonElement;
-export const deleteAllMissingDatabasesButton = document.getElementById("delete-all-missing-databases-button") as HTMLButtonElement;
-export const selectedMissingDatabaseDefault = document.getElementById("selected-missing-database-default") as HTMLOptionElement;
+export const navbarGhostDatabasesLink = document.getElementById("navbar-missing-databases") as HTMLAnchorElement;
+export const ghostDatabasesSelect = document.getElementById("selected-missing-database") as HTMLSelectElement;
+export const deleteGhostDatabaseButton = document.getElementById("delete-missing-database-button") as HTMLButtonElement;
+export const deleteAllGhostDatabasesButton = document.getElementById("delete-all-missing-databases-button") as HTMLButtonElement;
+export const selectedGhostDatabaseDefault = document.getElementById("selected-missing-database-default") as HTMLOptionElement;
 
 let databaseStrings: string[] = [];
 
+/**
+ * Interface that represents the body of the /rest/whoami/ response
+ */
 export interface Whoami {
     id: number;
     email: string;
     role: UserRole;
 }
 
+/**
+ * Interface that represents the body of the /rest/who/ response (more lightweight version of whoami that doesn't require DB calls)
+ */
 export interface Who {
     id: number,
     role: UserRole
 }
 
+/**
+ * Calls /rest/whoami and unwraps the data
+ * @returns A promise of the Whoami object (see [[Whoami]] )
+ */
 export async function getWhoamiPromise(): Promise<Whoami> {
     const response: AxiosResponse = await axios.get("/rest/whoami/");
     return response.data;
 }
 
+/**
+ * Calls /rest/who and unwraps the data
+ * @returns A promise of the Who object (see [[Who]] )
+ */
 export async function getWhoPromise(): Promise<Who> {
     const response: AxiosResponse = await axios.get("/rest/who/");
     return response.data;
 }
 
+/**
+ * Disables / enables the navbar elements
+ * @param enable Whether to enable or disable elements
+ */
 function changeNavbarState(enable: boolean): void {
     [navbarStudentView, navbarEditCourses, navbarEditUsers].forEach((element: HTMLLIElement) => {
         try {
@@ -73,17 +91,22 @@ function changeNavbarState(enable: boolean): void {
     });
 }
 
+/**
+ * Disables / enables elements in both the navbar and the current page
+ * @param enable Whether to enable or disable things
+ * @param callback The function that disables/enables things on the current page
+ */
 export function changePageState(enable: boolean, callback: Function): void {
-
-
     changeNavbarState(enable);
-
     callback(enable);
-
-
 }
 
-async function dumpAlldatabases(disableCallback: Function): Promise<boolean> {
+/**
+ * Makes a generate migrations script (if the user agrees to it)
+ * @param disableCallback The function that disables/enables all the elements on the current page
+ * @returns whether the operation was a success
+ */
+async function generateMigrations(disableCallback: Function): Promise<boolean> {
     const result = await Swal.fire({
         title: 'You are about to generate a migration shell script!',
         html: `Clicking migrate will create a shell script that generate a migration of all databases in the system.
@@ -122,38 +145,43 @@ async function dumpAlldatabases(disableCallback: Function): Promise<boolean> {
 
 }
 
-async function populateMissingDatabasesModal(): Promise<void> {
-    missingDatabasesSelect.disabled = true;
-    while (missingDatabasesSelect.lastElementChild !== missingDatabasesSelect.firstElementChild) {
-        const child: Element = missingDatabasesSelect.lastElementChild!;
-        missingDatabasesSelect.removeChild(child!);
+/**
+ * Populates the modal for deleting ghost databases (sets inputs and buttons to correct states and values)
+ */
+async function populateGhostDatabasesModal(): Promise<void> {
+    ghostDatabasesSelect.disabled = true;
+    while (ghostDatabasesSelect.lastElementChild !== ghostDatabasesSelect.firstElementChild) {
+        const child: Element = ghostDatabasesSelect.lastElementChild!;
+        ghostDatabasesSelect.removeChild(child!);
     }
 
     databaseStrings = (await axios.get("/rest/missing_databases/") as AxiosResponse<string[]>).data;
     if (databaseStrings.length === 0) {
-        deleteAllMissingDatabasesButton.disabled = true;
-        deleteMissingDatabaseButton.disabled = true;
-        selectedMissingDatabaseDefault.innerText = "There are no ghost databases!";
+        deleteAllGhostDatabasesButton.disabled = true;
+        deleteGhostDatabaseButton.disabled = true;
+        selectedGhostDatabaseDefault.innerText = "There are no ghost databases!";
     } else {
 
         for (let i = 1; i < databaseStrings.length + 1; i++) {
             const optionNode = document.createElement("option");
             optionNode.setAttribute("value", String(i));
             optionNode.appendChild(document.createTextNode(databaseStrings[i-1]));
-            missingDatabasesSelect.appendChild(optionNode);
+            ghostDatabasesSelect.appendChild(optionNode);
         }
-        missingDatabasesSelect.disabled = false;
-        deleteAllMissingDatabasesButton.disabled = false;
-        deleteMissingDatabaseButton.disabled = false;
-        selectedMissingDatabaseDefault.innerText = "Please select a database";
+        ghostDatabasesSelect.disabled = false;
+        deleteAllGhostDatabasesButton.disabled = false;
+        deleteGhostDatabaseButton.disabled = false;
+        selectedGhostDatabaseDefault.innerText = "Please select a database";
 
     }
-
-
-    return;
 }
 
-async function deleteAllMissingDatabases(disableCallback: Function): Promise<boolean> {
+/**
+ * Deletes all ghost databases if the user is sure they want this
+ * @param disableCallback The function that disables/enables all the elements on the current page
+ * @returns whether the operation was a success
+ */
+async function deleteAllGhostDatabases(disableCallback: Function): Promise<boolean> {
     const result = await Swal.fire({
         title: 'Are you sure you want to delete all these databases?',
         text: 'You will not be able to recover your data!',
@@ -179,14 +207,19 @@ async function deleteAllMissingDatabases(disableCallback: Function): Promise<boo
         success = false;
     } finally {
         changePageState(true, disableCallback);
-        populateMissingDatabasesModal();
+        populateGhostDatabasesModal();
 
     }
     return success;
 }
 
-async function deleteMissingDatabase(disableCallback: Function): Promise<boolean> {
-    if (!validSelect(missingDatabasesSelect)) {
+/**
+ * Deletes the selected ghost database, if the user is sure they want this
+ * @param disableCallback The function that disables/enables all the elements on the current page
+ * @returns whether the operation was a success
+ */
+async function deleteGhostDatabase(disableCallback: Function): Promise<boolean> {
+    if (!validSelect(ghostDatabasesSelect)) {
         return false;
     }
 
@@ -207,7 +240,7 @@ async function deleteMissingDatabase(disableCallback: Function): Promise<boolean
     const tempAlert: ChildNode | null = addTempAlert();
     changePageState(false, disableCallback);
     try {
-        const deleteConfig = {'data': [ databaseStrings[Number(missingDatabasesSelect.value)-1] ] };
+        const deleteConfig = {'data': [ databaseStrings[Number(ghostDatabasesSelect.value)-1] ] };
         await axios.delete(`/rest/missing_databases/`, deleteConfig); // TODO fix this call (body?)
         success = true;
     } catch (error) {
@@ -215,36 +248,40 @@ async function deleteMissingDatabase(disableCallback: Function): Promise<boolean
         success = false;
     } finally {
         changePageState(true, disableCallback);
-        populateMissingDatabasesModal();
+        populateGhostDatabasesModal();
 
     }
     return success;
 }
 
+/**
+ * Initiates all the event listeners with the proper callbacks (if the relevant elements exist, not all roles get to see the same elements)
+ * @param disableCallback The function that disables/enables all the elements on the current page
+ */
 export function initNavbar(disableCallback: Function): void {
     if (navbarDumpAllDatabasesLink) {
         navbarDumpAllDatabasesLink.addEventListener("click", (event) => {
             event.preventDefault();
-            dumpAlldatabases(disableCallback);
+            generateMigrations(disableCallback);
         })
     }
 
-    if (navbarMissingDatabasesLink) {
-        navbarMissingDatabasesLink.addEventListener("click", (event) => {
+    if (navbarGhostDatabasesLink) {
+        navbarGhostDatabasesLink.addEventListener("click", (event) => {
             event.preventDefault();
-            populateMissingDatabasesModal();
+            populateGhostDatabasesModal();
         })
     }
-        if (deleteAllMissingDatabasesButton) {
-            deleteAllMissingDatabasesButton.addEventListener("click",() => {
-                deleteAllMissingDatabases(disableCallback);
+        if (deleteAllGhostDatabasesButton) {
+            deleteAllGhostDatabasesButton.addEventListener("click",() => {
+                deleteAllGhostDatabases(disableCallback);
             });
 
         }
 
-        if (deleteMissingDatabaseButton) {
-            deleteMissingDatabaseButton.addEventListener("click", () => {
-                deleteMissingDatabase(disableCallback);
+        if (deleteGhostDatabaseButton) {
+            deleteGhostDatabaseButton.addEventListener("click", () => {
+                deleteGhostDatabase(disableCallback);
             })
         }
 
